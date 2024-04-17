0400d74f2dc0464c0da4f45cf604c5e5d5a39f99
Natalia Zheleznyak <109067095+natzhl@users.noreply.github.com>
2024-04-17 16:59:17 +0300

    Add files via upload

---
+++ video_persons_detection/main.py
@@ -0,0 +1,21 @@
+from imageai.Detection import VideoObjectDetection
+import os
+
+execution_path = os.getcwd() # путь к проекту
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(os.path.join(execution_path , "retinanet_resnet50_fpn_coco-eeacb38b.pth"))
+detector.loadModel()
+
+custom_objects = detector.CustomObjects(person=True)
+
+video_path = detector.detectObjectsFromVideo(
+    custom_objects=custom_objects,
+    input_file_path=os.path.join(execution_path, "crowd.mp4"),
+    output_file_path=os.path.join(execution_path, "crowd_person_detected"),
+    #custom_objects=custom,
+    frames_per_second=20, 
+    log_progress=True
+    )
+print(video_path)
